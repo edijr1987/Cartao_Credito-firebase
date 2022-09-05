697ffc54bd745eaf4898fbeb070c5621a7be586d
Edilson Júnior <98568723+edijr1987@users.noreply.github.com>
2022-09-05 09:48:28 -0300

    Create app.module.ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
+import { AppComponent } from './app.component';
+import { CriarCartaoComponent } from './components/criar-cartao/criar-cartao.component';
+import { ListarCartaoComponent } from './components/listar-cartao/listar-cartao.component';
+import { environment } from 'src/environments/environment';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CriarCartaoComponent,
+    ListarCartaoComponent,
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

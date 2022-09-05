059c059bef274cf26dcc16800d549794eb82b118
Edilson Júnior <98568723+edijr1987@users.noreply.github.com>
2022-09-05 09:50:26 -0300

    Create environment.prod.ts

---
+++ src/enviroments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBgcs7aoxge0EXL2BjORSL0NIms155lQg8",
+    authDomain: "cartaocredito-3a48b.firebaseapp.com",
+    projectId: "cartaocredito-3a48b",
+    storageBucket: "cartaocredito-3a48b.appspot.com",
+    messagingSenderId: "745539096040",
+    appId: "1:745539096040:web:24c76e90fd677b94d8685d"
+  }
+};
